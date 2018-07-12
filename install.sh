61d53b52415dd13bdcf8f45ce1d897a17d2aef84
Derrick Park <derrick8689@gmail.com>
2018-07-12 00:29:09 -0700

    Add install script

--- install.sh
+++ install.sh
@@ -2,5 +2,27 @@
 #
 # Perform Machine Setup.
 
-mv {.,} ~/ # move all files to $HOME
+#=====================
+# Remove old dot files
+#=====================
+sudo rm -rf ~/.vim > /dev/null 2>&1
+sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.bash_profile > /dev/null 2>&1
+sudo rm -rf ~/.git-completion.bash > /dev/null 2>&1
+sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+sudo rm -rf ~/.gitignore_global > /dev/null 2>&1
+sudo rm -rf ~/.g.sh > /dev/null 2>&1
 
+#=====================
+# Create symlinks in the home folder
+# (excluding .git/)
+#=====================
+for f in .[^.]*[^t]
+do
+    ln -s "$f" "~/$f"
+    echo "\nSymlinking $f to ~/$f"
+done
+
+echo "\n======= All Done!! =======\n"
+echo 
+echo "-Derrick"
